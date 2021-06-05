08673940d1af35ae23f6fef144fabfbd06210587
laukuansin <49442955+laukuansin@users.noreply.github.com>
2021-06-05 14:03:58 +0800

    Reminder
    -add reminder button
    -add background js, to check the due date of password

---
+++ background.js
@@ -0,0 +1,30 @@
+setInterval(checkPasswordDuration,1000);
+
+function checkPasswordDuration()
+{
+    browser.storage.local.get().then(function (result){
+        for(const key of Object.keys(result))
+        {
+            if(key.startsWith('Remind:'))
+            {
+                let current = new Date();
+                dueDate= new Date(result[key]['date']);
+                let leftDate=dueDate-current;
+                dueDate.setMonth(dueDate.getMonth()-3);
+                if(leftDate<=0)
+                {
+                    pwd=result[key]['password'];
+                    message="You have create "+pwd+" password at "+dueDate+", it has been over 3 month. It is a kindly reminder to remind you change a new password for the security purpose. "
+                    browser.notifications.create("tlpasswordgeneratornotification", {
+                        type: "basic",
+                        iconUrl: "images/password-64.png",
+                        title: "TL Password Generator",
+                        message,
+                    });
+
+                   browser.storage.local.remove(key);
+                }
+            }    
+        }
+    });
+}
